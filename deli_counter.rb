b0aded45afa0650f7deaba82c81f2ecc52be5953
Fabian Fabro <fabrofabian@gmail.com>
2019-04-20 06:34:37 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,10 +1,10 @@
 # Write your code here.
 katz_deli = []
 
-def line(katz_deli)
+def line(currentLine)
   lineCount = 1
 
-  if katz_deli.length == 0
+  if currentLine.length == 0
     return "The line is currently empty."
   else
     for person in katz_deli do
