a5095956be9a0bf8e544698815cded47d542571f
Fabian Fabro <fabrofabian@gmail.com>
2019-04-20 07:48:51 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,11 +3,11 @@ katz_deli = []
 
 def line(currentLine)
   lineCount = 1
-  lineOfPeople = "The line is currently: "
 
   if currentLine.length < 1
     puts "The line is currently empty."
   else
+    lineOfPeople = "The line is currently: "
     currentLine.each_with_index {|name, lineNum|
       recentLineNum = lineNum + 1
       lineOfPeople.concat"#{recentLineNum}. name "
