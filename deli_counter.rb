78ab74fa9e4902e4a65ac7a4ffa461ebcedcb9a1
Fabian Fabro <fabrofabian@gmail.com>
2019-04-20 06:32:16 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,13 @@
 # Write your code here.
+katz_deli = []
+
+def line(katz_deli)
+  lineCount = 1
+  
+  if katz_deli.length < 1
+    return "The line is currently empty."
+  else
+    for person in katz_deli do
+      
+    end
+end
